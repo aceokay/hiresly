6b991685051aa23899488dbb8c7949c9e7b616be
Austin <aokincaid@gmail.com>
2015-11-06 15:29:03 -0800

    Add custom method to determine if an employer can request a test from a developer

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,5 @@
 class Request < ActiveRecord::Base
-  # validates_uniqueness_of :user_id, :scope => :problem_id
+  validates_uniqueness_of :user_id, :scope => :problem_id
   belongs_to :user
   belongs_to :problem
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,16 @@ class User < ActiveRecord::Base
   has_many :interests, class_name: "User",
                        foreign_key: "interested_user_id"
   belongs_to :interested_user, class_name: "User"
+
+  def requestable(employer)
+    show_request_link = true
+    if self.requests.any?
+      self.requests.each do |request|
+        if employer.problems.where({ id: request.problem_id })
+          show_request_link = false
+        end
+      end
+    end
+    show_request_link
+  end
 end
