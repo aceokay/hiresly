cd8be0a4cfd1a6c5986fbb58eab8223e154189af
Austin <aokincaid@gmail.com>
2015-11-30 22:49:16 -0800

    Add beginning of seed file for MVP presentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+developers = [
+  ["Austin Kincaid", "fake1@email.com", "asdasdasd", "https://www.linkedin.com/in/aokincaid", "https://github.com/aceokay", "http://aceokay.github.io/"]
+  ["Carlo De Gregorio", "fake2@email.com", "asdasdasd", "https://www.linkedin.com/in/carlodegregorio", "https://github.com/cdg77", "http://cdg77.github.io/portfolio/"]
+  ["Jessica Joly", "fake3@email.com", "asdasdasd", "https://www.linkedin.com/in/jessicajoly", "https://github.com/jessicajoly88", "https://github.com/jessicajoly88"]
+  ["Alisa Colón", "fake4@email.com", "asdasdasd", "https://www.linkedin.com/in/alisacolon", "https://github.com/alisacodes", "https://github.com/alisacodes"]
+  ["Jennifer Bell", "fake5@email.com", "asdasdasd", "https://www.linkedin.com/in/jenleebell", "https://github.com/jenleebell", "https://github.com/jenleebell"]
+  ["Jake Glassman", "fake6@email.com", "asdasdasd", "https://www.linkedin.com/in/jakeglassman", "https://github.com/theglassman32", "https://github.com/theglassman32"]
+]
+
+developers.each do |name, email, password, linkedin, github, website|
+  User.create(name: name, email: email, password: password, linkedin: linkedin, github: github, website: website)
+end
+
+employers = [
+  []
+]
